825c7e159585aec784332dac8fdf144310148f48
Amornthep.ra <amornthep064@gmail.com>
2026-04-03 10:11:29 +0700

    Initial modified board version

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 module.exports = {
-  name: "PuppyBot6",
+  name: "Modified_Block_version",
   platform: "arduino-rp2350",
-  title: "Modified_Block_version",
+  title: "PuppyBot6-Modified",
   description: "ไฟล์ที่เซฟไว้จะไม่สามารถเปิดกับเวอร์ชันนี้ได้ เนื่องจากมีการปรับปรุงบล็อกคำสั่งใหม่",
   author: "PrinceBot",
   website: "https://princebot.net",
   email: "print081@gmail.com",
-  git: "https://github.com/PrinceBot-Ratthanin/kbpro-puppybot-board",
+  git: "https://github.com/Amornthep-ra/PuppyBot6_Modified_Block_version.git",
   download_url:
-    "https://woottinan.github.io/kbide-board-repo/boards/PuppyBot6_Modified_Block_version/PuppyBot6_Modified_Block_version.zip",
+    "https://github.com/Amornthep-ra/PuppyBot6_Modified_Block_version/archive/refs/heads/main.zip",
   image: "/static/cover.jpg",
   version: "1.0.0",
   fqbn: "rp2040:rp2040:PuppyBot6_Modified_Block_version",
   corePath: "arduino-rp2350",
 };
-
-
--- include/KB_PIDFollowHelpers.h
+++ include/KB_PIDFollowHelpers.h
@@ -12,25 +12,32 @@ static inline void follow_line_by_encoder_safe_reader(int driveMode,
                                                       float kd,
                                                       long targetPulses,
                                                       long timeoutMs,
-                                                      ReadEncFn readEnc) {
+                                                      ReadEncFn readEnc)
+{
   long base = readEnc();
   long target = labs(targetPulses);
   unsigned long start = millis();
   bool timedOut = false;
 
-  while (labs(readEnc() - base) < target) {
-    if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs) {
+  while (labs(readEnc() - base) < target)
+  {
+    if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs)
+    {
       timedOut = true;
       break;
     }
-    if (driveMode == 1) {
+    if (driveMode == 1)
+    {
       Run_PID4DW(signedSpeed, kp, kd);
-    } else {
+    }
+    else
+    {
       Run_PID(signedSpeed, kp, kd);
     }
   }
   ao();
-  if (!timedOut) delay(50);
+  if (!timedOut)
+    delay(50);
 }
 
 template <typename ReadEncFn>
@@ -42,8 +49,10 @@ static inline void follow_line_by_encoder_store_safe_reader(int driveMode,
                                                             long timeoutMs,
                                                             bool resetEnc,
                                                             ReadEncFn readEnc,
-                                                            long* outPulses) {
-  if (resetEnc) {
+                                                            long *outPulses)
+{
+  if (resetEnc)
+  {
     resetEncoder(selection_Encoder);
   }
   long base = readEnc();
@@ -51,22 +60,29 @@ static inline void follow_line_by_encoder_store_safe_reader(int driveMode,
   unsigned long start = millis();
   bool timedOut = false;
 
-  while (labs(readEnc() - base) < target) {
-    if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs) {
+  while (labs(readEnc() - base) < target)
+  {
+    if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs)
+    {
       timedOut = true;
       break;
     }
-    if (driveMode == 1) {
+    if (driveMode == 1)
+    {
       Run_PID4DW(signedSpeed, kp, kd);
-    } else {
+    }
+    else
+    {
       Run_PID(signedSpeed, kp, kd);
     }
   }
   long finalEnc = readEnc();
   long traveled = labs(finalEnc - base);
   ao();
-  if (!timedOut) delay(50);
-  if (outPulses) *outPulses = traveled;
+  if (!timedOut)
+    delay(50);
+  if (outPulses)
+    *outPulses = traveled;
 }
 
 template <typename ReadEncFn>
@@ -81,8 +97,10 @@ static inline void follow_line_cross_safe_reader(int driveMode,
                                                  long stopDelay,
                                                  long timeoutMs,
                                                  bool resetEnc,
-                                                 ReadEncFn readEnc) {
-  if (resetEnc) {
+                                                 ReadEncFn readEnc)
+{
+  if (resetEnc)
+  {
     resetEncoder(selection_Encoder);
   }
   int count = 0;
@@ -90,46 +108,66 @@ static inline void follow_line_cross_safe_reader(int driveMode,
   unsigned long start = millis();
   bool timedOut = false;
 
-  while (count < crosses) {
-    if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs) {
+  while (count < crosses)
+  {
+    if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs)
+    {
       timedOut = true;
       break;
     }
-    if (driveMode == 1) {
+    if (driveMode == 1)
+    {
       Run_PID4DW(signedSpeed, kp, kd);
-    } else {
+    }
+    else
+    {
       Run_PID(signedSpeed, kp, kd);
     }
-    if (Read_sumValue_sensor() > sumThreshold) {
+    if (Read_sumValue_sensor() > sumThreshold)
+    {
       count++;
       delay(50);
     }
   }
 
-  if (!timedOut) {
-    if (extraMode == KB_EXTRA_ENC) {
+  if (!timedOut)
+  {
+    if (extraMode == KB_EXTRA_ENC)
+    {
       long base2 = readEnc();
-      while (labs(readEnc() - base2) < extraVal) {
-        if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs) {
+      while (labs(readEnc() - base2) < extraVal)
+      {
+        if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs)
+        {
           timedOut = true;
           break;
         }
-        if (driveMode == 1) {
+        if (driveMode == 1)
+        {
           Run_PID4DW(signedSpeed, kp, kd);
-        } else {
+        }
+        else
+        {
           Run_PID(signedSpeed, kp, kd);
         }
       }
-    } else {
+    }
+    else
+    {
       unsigned long extraStart = millis();
-      while ((millis() - extraStart) < (unsigned long)extraVal) {
-        if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs) {
+      while ((millis() - extraStart) < (unsigned long)extraVal)
+      {
+        if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs)
+        {
           timedOut = true;
           break;
         }
-        if (driveMode == 1) {
+        if (driveMode == 1)
+        {
           Run_PID4DW(signedSpeed, kp, kd);
-        } else {
+        }
+        else
+        {
           Run_PID(signedSpeed, kp, kd);
         }
       }
@@ -137,7 +175,8 @@ static inline void follow_line_cross_safe_reader(int driveMode,
   }
 
   ao();
-  if (!timedOut) delay(stopDelay);
+  if (!timedOut)
+    delay(stopDelay);
 }
 
 // Run PID line follow until target pulses (with timeout)
@@ -150,7 +189,8 @@ static inline void follow_line_by_encoder_safe(int driveMode,
                                                float kd,
                                                long targetPulses,
                                                long timeoutMs,
-                                               long (*readEnc)()) {
+                                               long (*readEnc)())
+{
   follow_line_by_encoder_safe_reader(driveMode, signedSpeed, kp, kd, targetPulses, timeoutMs, readEnc);
 }
 
@@ -163,7 +203,8 @@ static inline void follow_line_by_encoder_store_safe(int driveMode,
                                                      long timeoutMs,
                                                      bool resetEnc,
                                                      long (*readEnc)(),
-                                                     long* outPulses) {
+                                                     long *outPulses)
+{
   follow_line_by_encoder_store_safe_reader(driveMode, signedSpeed, kp, kd, targetPulses, timeoutMs, resetEnc, readEnc, outPulses);
 }
 
@@ -179,7 +220,8 @@ static inline void follow_line_cross_safe(int driveMode,
                                           long stopDelay,
                                           long timeoutMs,
                                           bool resetEnc,
-                                          long (*readEnc)()) {
+                                          long (*readEnc)())
+{
   follow_line_cross_safe_reader(driveMode, signedSpeed, kp, kd, sumThreshold, crosses, extraVal, extraMode, stopDelay, timeoutMs, resetEnc, readEnc);
 }
 
@@ -188,31 +230,48 @@ static inline void line_reacquire_safe(int sideMode,
                                        int spinMode,
                                        int searchSpeed,
                                        long sumThreshold,
-                                       long timeoutMs) {
+                                       long timeoutMs)
+{
   unsigned long start = millis();
   int speed = abs(searchSpeed);
-  while (true) {
-    if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs) {
+  while (true)
+  {
+    if (timeoutMs > 0 && (millis() - start) >= (unsigned long)timeoutMs)
+    {
       break;
     }
     int sumValue = (sideMode == 1) ? Read_sumValue_sensor_B() : Read_sumValue_sensor();
-    if (sumValue >= sumThreshold) {
+    if (sumValue >= sumThreshold)
+    {
       break;
     }
 
-    if (driveMode == 1) {
-      if (spinMode == 1) {
-        motor(1, speed);  motor(2, -speed);
-        motor(3, speed);  motor(4, -speed);
-      } else {
-        motor(1, -speed); motor(2, speed);
-        motor(3, -speed); motor(4, speed);
-      }
-    } else {
-      if (spinMode == 1) {
+    if (driveMode == 1)
+    {
+      if (spinMode == 1)
+      {
         motor(1, speed);
         motor(2, -speed);
-      } else {
+        motor(3, speed);
+        motor(4, -speed);
+      }
+      else
+      {
+        motor(1, -speed);
+        motor(2, speed);
+        motor(3, -speed);
+        motor(4, speed);
+      }
+    }
+    else
+    {
+      if (spinMode == 1)
+      {
+        motor(1, speed);
+        motor(2, -speed);
+      }
+      else
+      {
         motor(1, -speed);
         motor(2, speed);
       }
