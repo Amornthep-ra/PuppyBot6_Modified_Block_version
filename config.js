5c18005054eb85abb1cb47c1545914ad229e0bdf
Amornthep.ra <amornthep064@gmail.com>
2026-04-03 10:35:27 +0700

    Initial modified board version

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ module.exports = {
   website: "https://princebot.net",
   email: "print081@gmail.com",
   git: "https://github.com/Amornthep-ra/PuppyBot6_Modified_Block_version.git",
-  download_url:
-    "https://raw.githubusercontent.com/Amornthep-ra/PuppyBot6_Modified_Block_version/main/PuppyBot6_Modified_Block_version.zip",
+  download_url:`r`n    "https://raw.githubusercontent.com/Amornthep-ra/PuppyBot6_Modified_Block_version_zip/main/PuppyBot6_Modified_Block_version.zip",
   image: "/static/cover.jpg",
   version: "1.0.0",
   fqbn: "rp2040:rp2040:PuppyBot6_Modified_Block_version",
   corePath: "arduino-rp2350",
 };
+
