ccc127b82e17e1bfe895c436037ef2b7c4c16d18
Amornthep.ra <amornthep064@gmail.com>
2026-04-03 10:44:19 +0700

    Initial modified board version

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   email: "print081@gmail.com",
   git: "https://github.com/Amornthep-ra/PuppyBot6_Modified_Block_version.git",
   download_url:
-    "https://raw.githubusercontent.com/Amornthep-ra/PuppyBot6_Modified_Block_version_zip/main/PuppyBot6_Modified_Block_version.zip",
+    "https://github.com/Amornthep-ra/PuppyBot6_Modified_Block_version/releases/download/v1.0.0/PuppyBot6_Modified_Block_version.zip",
   image: "/static/cover.jpg",
   version: "1.0.0",
   fqbn: "rp2040:rp2040:PuppyBot6_Modified_Block_version",
